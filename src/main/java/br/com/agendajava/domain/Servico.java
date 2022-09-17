e15d8f4c2009d28ffb91a277ba9b648c3a1b0622
andersonsilvasw <andersonsilvaedif@hotmail.com>
2022-09-17 17:25:08 -0300

    Mapamento da entidade Serviço

--- src/main/java/br/com/agendajava/domain/Servico.java
+++ src/main/java/br/com/agendajava/domain/Servico.java
@@ -1,91 +1,41 @@
 package br.com.agendajava.domain;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "servico")
+@Data
 public class Servico {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String descricao;
-	private Date data_do_servico;
-	private Date data_do_pagamento;
-	private Double valor;
-	private String forma_de_pagamento;
-	private String observacao;
 	
-	public Servico() {
-	}
-
-	public Servico(Long id, String descricao, Date data_do_servico, Date data_do_pagamento, Double valor,
-			String forma_de_pagamento, String observacao) {
-		this.id = id;
-		this.descricao = descricao;
-		this.data_do_servico = data_do_servico;
-		this.data_do_pagamento = data_do_pagamento;
-		this.valor = valor;
-		this.forma_de_pagamento = forma_de_pagamento;
-		this.observacao = observacao;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public Date getData_do_servico() {
-		return data_do_servico;
-	}
-
-	public void setData_do_servico(Date data_do_servico) {
-		this.data_do_servico = data_do_servico;
-	}
-
-	public Date getData_do_pagamento() {
-		return data_do_pagamento;
-	}
-
-	public void setData_do_pagamento(Date data_do_pagamento) {
-		this.data_do_pagamento = data_do_pagamento;
-	}
-
-	public Double getValor() {
-		return valor;
-	}
-
-	public void setValor(Double valor) {
-		this.valor = valor;
-	}
-
-	public String getForma_de_pagamento() {
-		return forma_de_pagamento;
-	}
-
-	public void setForma_de_pagamento(String forma_de_pagamento) {
-		this.forma_de_pagamento = forma_de_pagamento;
-	}
-
-	public String getObservacao() {
-		return observacao;
-	}
-
-	public void setObservacao(String observacao) {
-		this.observacao = observacao;
-	}
-
-	@Override
-	public String toString() {
-		return "Servico [id=" + id + ", descricao=" + descricao + ", data_do_servico=" + data_do_servico
-				+ ", data_do_pagamento=" + data_do_pagamento + ", valor=" + valor + ", forma_de_pagamento="
-				+ forma_de_pagamento + ", observacao=" + observacao + "]";
-	}
+	@Column(name = "descricao", nullable = false, length = 60)
+	private String descricao;
+	
+	@Column(name = "data_do_servico")
+	private Date data_do_servico;
+	
+	@Column(name = "data_do_pagamento")
+	private Date data_do_pagamento;
+	
+	@Column(name = "valor", precision = 7, scale = 2, nullable = false)
+	private BigDecimal valor;
+	
+	@Column(name = "forma_de_pagamento", nullable = false, length = 40)
+	private String forma_de_pagamento;
+	
+	@Column(name = "observacao", length = 100)
+	private String observacao;
 }
