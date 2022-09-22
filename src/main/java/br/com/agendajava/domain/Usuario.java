253bf7d3f0302a882dea412106c80d31d88fac89
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-09-21 21:48:49 -0300

    Refatoração da entidade usuário

--- src/main/java/br/com/agendajava/domain/Usuario.java
+++ src/main/java/br/com/agendajava/domain/Usuario.java
@@ -2,20 +2,18 @@ package br.com.agendajava.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Entity
 @Table(name = "usuario")
 @Data
-public class Usuario {
+@EqualsAndHashCode(callSuper = false)
+public class Usuario extends AbstractEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private static final long serialVersionUID = -4575202674401302951L;
 
 	@Column(name = "nome", nullable = false, length = 60)
 	private String nome;
@@ -25,5 +23,15 @@ public class Usuario {
 
 	@Column(name = "password", nullable = false, length = 8)
 	private String password;
+	
+	public Usuario() {
+		
+	}
+
+	public Usuario(String nome, String login, String password) {
+		this.nome = nome;
+		this.login = login;
+		this.password = password;
+	}
 
 }
